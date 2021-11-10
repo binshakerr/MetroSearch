9c03fba952275622f809223364f67bc874dc7313
binshakerr-7peaks <eslam.shaker@7peakssoftware.com>
2021-11-10 15:48:26 +0200

    domain entities

---
+++ MetroSearch/MetroSearch/Domain/Entities/MuseumItem.swift
@@ -0,0 +1,21 @@
+//
+//  MusuemItem.swift
+//  MetroSearch
+//
+//  Created by Eslam Shaker on 10/11/2021.
+//
+
+import Foundation
+
+struct MuseumItem: Codable {
+    var objectID: Int
+    var primaryImage: String?
+    var primaryImageSmall: String?
+    var additionalImages: [String]?
+    var title: String?
+    var artistDisplayName: String?
+    var artistDisplayBio: String?
+    var medium: String?
+    var dimensions: String?
+    var objectDate: String?
+}
---
+++ MetroSearch/MetroSearch/Domain/Entities/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  MetroSearch
+//
+//  Created by Eslam Shaker on 10/11/2021.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var total: Int
+    var objectIDs: [Int]
+}
