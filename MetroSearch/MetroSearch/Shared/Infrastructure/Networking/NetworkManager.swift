5cb8cbd5607a29f547748de61143cd8ac68408b4
Eslam Shaker <binshakerr@gmail.com>
2022-02-13 20:48:19 +0200

    network manager improvement

--- MetroSearch/MetroSearch/Shared/Infrastructure/Networking/NetworkManager.swift
+++ MetroSearch/MetroSearch/Shared/Infrastructure/Networking/NetworkManager.swift
@@ -12,7 +12,7 @@ var customSessionManager: Session = {
     let configuration = URLSessionConfiguration.af.default
     configuration.timeoutIntervalForRequest = 30
     configuration.waitsForConnectivity = true
-
+    
     let networkLogger = NetworkLogger()
     return Session(
         configuration: configuration,
@@ -26,12 +26,10 @@ protocol NetworkManagerType {
 class NetworkManager {
     
     private let session: Session
-    private let requiresValidation: Bool
-    static let shared = NetworkManager(session: customSessionManager, requiresValidation: true)
+    static let shared = NetworkManager(session: customSessionManager)
     
-    init(session: Session, requiresValidation: Bool) {
+    init(session: Session) {
         self.session = session
-        self.requiresValidation = requiresValidation
     }
 }
 
@@ -39,9 +37,7 @@ extension NetworkManager: NetworkManagerType {
     
     func request<T: Decodable>(_ request: URLRequestConvertible, type: T.Type, completion: @escaping(Result<T, Error>) -> Void) {
         
-        var dataRequest: DataRequest!
-        
-        // 1- Check Internet Connection
+        // Check Internet Connection
         if NetworkReachability.shared.status == .notReachable {
             let userInfo: [String : Any] = [
                 NSLocalizedDescriptionKey:  NSLocalizedString("Connection", value: "Please check your internet connectivity", comment: "") ,
@@ -51,43 +47,41 @@ extension NetworkManager: NetworkManagerType {
             completion(.failure(error))
         }
         
-        // 2- Validate Request
-        dataRequest = requiresValidation ? customSessionManager.request(request).validate() : customSessionManager.request(request)
-        
-        // 3- Parse Request
-        dataRequest.responseJSON { result in
-            let statusCode = result.response?.statusCode ?? 0
-            
-            switch result.result {
-            case .success(let response):
-                switch statusCode {
-                    
-                case 200...299:
-                    //see if it's error first
-                    do {
-                        let jsonData = try JSONSerialization.data(withJSONObject: response, options: .prettyPrinted)
-                        let result = try JSONDecoder().decode(type.self, from: jsonData)
-                        completion(.success(result))
-                    } catch let error {
-                        completion(.failure(error))
-                    }
-                default: // Custom error
-                    do {
-                        let jsonData = try JSONSerialization.data(withJSONObject: response, options: .prettyPrinted)
-                        let result = try JSONDecoder().decode(ErrorResponse.self, from: jsonData)
-                        let userInfo: [String : Any] = [
-                            NSLocalizedDescriptionKey:  NSLocalizedString("Error code: \(statusCode)", value: result.message, comment: "") ,
-                        ]
-                        let error = NSError(domain: "", code: 0, userInfo: userInfo)
-                        completion(.failure(error))
-                    } catch let error {
-                        completion(.failure(error))
-                    }
-                }
+        // Make Request
+        session
+            .request(request)
+            .validate()
+            .responseJSON { result in
+                let statusCode = result.response?.statusCode ?? 0
                 
-            case .failure(let error):
-                completion(.failure(error))
+                switch result.result {
+                case .success:
+                    switch statusCode {
+                    case 200...299:
+                        do {
+                            guard let data = result.data else { return }
+                            let result = try JSONDecoder().decode(type.self, from: data)
+                            completion(.success(result))
+                        } catch let error {
+                            completion(.failure(error))
+                        }
+                    default: // Custom error
+                        do {
+                            guard let data = result.data else { return }
+                            let result = try JSONDecoder().decode(ErrorResponse.self, from: data)
+                            let userInfo: [String : Any] = [
+                                NSLocalizedDescriptionKey:  NSLocalizedString("Error code: \(statusCode)", value: result.message, comment: "") ,
+                            ]
+                            let error = NSError(domain: "", code: 0, userInfo: userInfo)
+                            completion(.failure(error))
+                        } catch let error {
+                            completion(.failure(error))
+                        }
+                    }
+                    
+                case .failure(let error):
+                    completion(.failure(error))
+                }
             }
-        }
     }
 }
